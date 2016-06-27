b856ace16bd274f9d765df8e61bde2231f3b938e
Ox Cart <ox.to.a.cart@gmail.com>
2016-06-27 18:40:17 -0500

    Working with test

---
+++ vtime.go
@@ -0,0 +1,90 @@
+package vtime
+
+import (
+	"sync"
+	"time"
+)
+
+type Clock struct {
+	now     time.Time
+	tickers map[int]*Ticker
+	mx      sync.RWMutex
+}
+
+func NewClock(now time.Time) *Clock {
+	return &Clock{
+		now:     now,
+		tickers: make(map[int]*Ticker, 0),
+	}
+}
+
+type Ticker struct {
+	id   int
+	cl   *Clock
+	d    time.Duration
+	last time.Time
+	u    chan time.Time
+	c    chan time.Time
+	C    <-chan time.Time
+}
+
+func (cl *Clock) Advance(t time.Time) {
+	cl.mx.Lock()
+	if t.After(cl.now) {
+		cl.now = t
+		for _, tk := range cl.tickers {
+			tk.advance(t)
+		}
+	}
+	cl.mx.Unlock()
+}
+
+func (cl *Clock) Now() time.Time {
+	cl.mx.RLock()
+	now := cl.now
+	cl.mx.RUnlock()
+	return now
+}
+
+func (cl *Clock) NewTicker(d time.Duration) *Ticker {
+	cl.mx.Lock()
+	tk := &Ticker{
+		id:   len(cl.tickers),
+		d:    d,
+		cl:   cl,
+		last: cl.now,
+		u:    make(chan time.Time),
+		c:    make(chan time.Time, 1),
+	}
+	tk.C = tk.c
+	cl.tickers[tk.id] = tk
+	cl.mx.Unlock()
+	go tk.run()
+	return tk
+}
+
+func (tk *Ticker) advance(now time.Time) {
+	tk.u <- now
+}
+
+func (tk *Ticker) run() {
+	for now := range tk.u {
+		if !tk.last.IsZero() && now.Sub(tk.last) >= tk.d {
+			tk.last = now
+			// New tick
+			select {
+			case tk.c <- now:
+				// submitted tick
+			default:
+				// no on listening, drop it on the floor
+			}
+		}
+	}
+}
+
+func (tk *Ticker) Stop() {
+	tk.cl.mx.Lock()
+	delete(tk.cl.tickers, tk.id)
+	close(tk.u)
+	tk.cl.mx.Unlock()
+}
---
+++ vtime_test.go
@@ -0,0 +1,31 @@
+package vtime
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTicker(t *testing.T) {
+	now := time.Date(2015, 5, 6, 7, 8, 9, 10, time.UTC)
+	cl := NewClock(now)
+	tk := cl.NewTicker(5 * time.Millisecond)
+
+	readTick := func() time.Time {
+		select {
+		case t := <-tk.C:
+			return t
+		default:
+			return time.Time{}
+		}
+	}
+
+	for i := 0; i <= 10; i++ {
+		if i < 5 {
+			assert.Equal(t, time.Time{}, readTick())
+		}
+		cl.Advance(now.Add(time.Duration(i) * time.Millisecond))
+	}
+	assert.Equal(t, now.Add(5*time.Millisecond), readTick())
+}
